3a7b0113cf385ba9cb4a92e652a7de93cb8f66fd
yuan310979 <chc71340@gmail.com>
2019-01-31 17:17:40 +0800

    add functionality to process IAQI scale

--- epainfo/epainfo.py
+++ epainfo/epainfo.py
@@ -1,5 +1,6 @@
 import pickle
 import re
+import iaqi
 
 from math import sin, cos, sqrt, atan2, radians, degrees, isnan
 from pathlib import Path
@@ -38,8 +39,43 @@ class EPAInfo:
         ret.append(self.latlon_distance(lat, lon, st['lat'], st['lon']))
         return ret
 
+    def moving_average(self, data, aqi_type, date, hour, delta):
+        sum = 0.0
+        prev_val = 0
+        ed_hour = hour
+        delta = delta - hour
+        count = 0
+        while delta > 0:
+            for _t in range(0, ed_hour): 
+                i = data[date][aqi_type][_t]
+                if isinstance(i, int) or isinstance(i, float):
+                    if isnan(i):
+                        i = prev_val
+                elif isinstance(i, str):
+                    i = float(re.findall('[0-9]+[\.]{0,1}[0-9]*', i)[0])
+                else:
+                    i = prev_val
+                count += 1
+                sum += i
+            ed_hour = 24
+            delta -= 24
+            date -= timedelta(days=1)
+        for _t in range(-delta, ed_hour):
+            i = data[date][aqi_type][_t]
+            if isinstance(i, int) or isinstance(i, float):
+                if isnan(i):
+                    i = prev_val
+            elif isinstance(i, str):
+                i = float(re.findall('[0-9]+[\.]{0,1}[0-9]*', i)[0])
+            else:
+                i = prev_val
+            sum += i
+            count += 1
+        return sum / count 
+
     def get_aqi_data_in_period(self, st_name, aqi_types, st_time, ed_time):
         aqi = ['CO', 'SO2', 'NO', 'NO2', 'NOx', 'O3', 'PM2.5', 'PM10']
+        iaqi_type = ['CO', 'SO2', 'O3', 'NO2', 'PM2.5', 'PM10']
         st_date = st_time.date()
         ed_date = ed_time.date()
         st_hour = st_time.hour
@@ -47,6 +83,7 @@ class EPAInfo:
         ret = [] 
         d = self.get_data_by_station_name(st_name)
         prev_val = [0 for _ in range(len(aqi_types))] 
+        IAQI = iaqi.IAQI()
         #  with tqdm(total=(ed_date-st_date)/timedelta(days=1)) as pbar:
         while st_date != ed_date + timedelta(days=1):
             try:
@@ -63,12 +100,29 @@ class EPAInfo:
                         if isinstance(i, int) or isinstance(i, float):
                             if isnan(i):
                                 i = prev_val[aqi_index]
-                            tmp.append(i)
                         elif isinstance(i, str):
                             i = float(re.findall('[0-9]+[\.]{0,1}[0-9]*', i)[0])
-                            tmp.append(i)
                         else:
                             print('[Error]\t(get_aqi_data_in_period) value error!')
+                        
+                        if aqi_type == 'O3':
+                            O3_8hr = self.moving_average(d, aqi_type, st_date, t, 8) / 1000
+                            O3_hr = i / 1000
+                            i = IAQI.transfer2IAQI(aqi_type, O3_hr, O3_8hr)
+                        elif aqi_type == 'PM2.5':
+                            i = self.moving_average(d, aqi_type, st_date, t, 16)
+                            i = IAQI.transfer2IAQI(aqi_type, i)
+                        elif aqi_type == 'PM10':
+                            i = self.moving_average(d, aqi_type, st_date, t, 16)
+                            i = IAQI.transfer2IAQI(aqi_type, i)
+                        elif aqi_type == 'CO':
+                            i = self.moving_average(d, aqi_type, st_date, t, 8)
+                            i = IAQI.transfer2IAQI(aqi_type, i)
+                        elif aqi_type == 'SO2':
+                            i = IAQI.transfer2IAQI(aqi_type, i)
+                        elif aqi_type == 'NO2':
+                            i = IAQI.transfer2IAQI(aqi_type, i)
+                        tmp.append(i)
                         prev_val[aqi_index] = i
                     ret.append(tmp)
             except Exception as ex:
@@ -119,6 +173,6 @@ if __name__ == "__main__":
     st = EPA.get_nearest_station(24.32323, 121.456465)
     #  print(EPA.get_data_by_station_name(st))
     print(st)
-    d = EPA.get_aqi_data_in_period(st, ['PM2.5', 'SO2', 'PM10'], datetime(2015, 10, 1, 1, 0), datetime(2015, 10, 26, 2, 0))
+    d = EPA.get_aqi_data_in_period(st, ['PM2.5', 'SO2', 'PM10', 'CO', 'NO2', 'O3'], datetime(2015, 10, 1, 1, 0), datetime(2015, 10, 26, 2, 0))
     pp(d)
     pp(len(d))
---
+++ epainfo/iaqi.py
@@ -0,0 +1,51 @@
+class IAQI:
+    def __init__(self):
+        # Corresponding IAQI chart
+        self.O3 = [0, 0, 0.125, 0.164, 0.204, 0.404, 0.504, 0.604]
+        self.O3_8hr = [0, 0.054, 0.07, 0.085, 0.105, 0.20, 0.20, 0.20]
+        self.PM25 = [0, 15.4, 35.4, 54.4, 150.4, 250.4, 350.4, 500.4]
+        self.PM10 = [0, 54, 125, 254, 354, 424, 504, 604]
+        self.CO = [0, 4.4, 9.4, 12.4, 15.4, 30.4, 40.4, 50.4]
+        self.SO2 = [0, 35, 75, 185, 304, 604, 804, 1004]
+        self.NO2 = [0, 53, 100, 360, 649, 1249, 1649, 2049]
+
+    def get_iaqi_params(self, target, _val):
+        index = -1
+        for idx, val in enumerate(target):
+            if _val <= val:
+                index = idx
+                break
+        return _val, target[idx-1], target[idx], 50*(idx-1), 50*idx 
+
+    def transfer2IAQI(self, target, *args):
+        if target == "O3":
+            O3_hr, O3_8hr = args
+            val1, val2 = 0, 0
+            if O3_hr >= self.O3[2]:
+                val1 = self.IAQI_formula(*self.get_iaqi_params(self.O3, O3_hr))
+            if O3_8hr <= self.O3_8hr[-3]:
+                val2 = self.IAQI_formula(*self.get_iaqi_params(self.O3_8hr, O3_8hr))
+            print(val1, val2)
+            return val1 if val1 > val2 else val2
+        elif target == "PM2.5":
+            return self.IAQI_formula(*self.get_iaqi_params(self.PM25, args[0]))
+        elif target == "PM10":
+            return self.IAQI_formula(*self.get_iaqi_params(self.PM10, args[0]))
+        elif target == "CO":
+            return self.IAQI_formula(*self.get_iaqi_params(self.CO, args[0]))
+        elif target == "SO2":
+            return self.IAQI_formula(*self.get_iaqi_params(self.SO2, args[0]))
+        elif target == "NO2":
+            return self.IAQI_formula(*self.get_iaqi_params(self.NO2, args[0]))
+        else:
+            print("[Error] Pollution type not found.")
+            exit()
+
+    @staticmethod
+    def IAQI_formula(Cp, BPhi, BPlo, IAQIh, IAQIl):
+        return (IAQIh-IAQIl) / (BPhi-BPlo) * (Cp-BPlo) + IAQIl
+
+if __name__ == '__main__':
+    m = IAQI()
+    v = m.transfer2IAQI('O3', 0.52, 0.1)
+    print(v)
