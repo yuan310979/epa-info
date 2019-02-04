7d9a131c120538ccb38ad6cbdd962189dddaa5f4
yuan310979 <chc71340@gmail.com>
2019-02-05 02:26:30 +0800

    prevent the condition of which some record may have missing row. ex: 2010/08/08 中崙 has no SO2 data.

--- epainfo/build_station_data.py
+++ epainfo/build_station_data.py
@@ -26,6 +26,29 @@ def build_station_data(year:str, ret:Dict) -> Dict[str, Dict[datetime, Dict[str,
 
     assert ftype != 'ods'
     
+    attr = ['AMB_TEMP',
+            'CO',
+            'NO',
+            'NO2',
+            'NOx',
+            'O3',
+            'PH_RAIN',
+            'PM10',
+            'PM2.5',
+            'RAINFALL',
+            'RAIN_COND',
+            'RH',
+            'SO2',
+            'WD_HR',
+            'WIND_DIREC',
+            'WIND_SPEED',
+            'WS_HR']
+
+    record = {}
+    for _attr in attr:
+        record[_attr] = [-1] * 24
+
+    _date = None
     target_path = sorted(dataset_path.glob(f'**/*.{ftype}'))
     if ftype == 'xls':
         for p in tqdm(target_path):
@@ -34,14 +57,22 @@ def build_station_data(year:str, ret:Dict) -> Dict[str, Dict[datetime, Dict[str,
                 ret[station_name] = {}
             xls = pd.read_excel(p)
             for row in xls.iterrows():
+                date = None
                 if(isinstance(row[1][0], datetime)):
                     date = datetime.strptime(str(row[1][0]), "%Y-%m-%d %H:%M:%S").date()
                 else:
                     date = datetime.strptime(row[1][0], "%Y/%m/%d").date() 
+                if _date != date:
+                    record = {}
+                    for _attr in attr:
+                        record[_attr] = [-1] * 24
                 key = row[1][2]
-                if(ret.get(station_name).get(date) == None):
-                    ret[station_name][date] = {}
-                ret[station_name][date][key] = list(row[1][3:])
+                record[key] = list(row[1][3:])
+                ret[station_name][date] = record
+                #  if(ret.get(station_name).get(date) == None):
+                    #  ret[station_name][date] = {}
+                #  ret[station_name][date][key] = list(row[1][3:])
+                _date = date
     elif ftype == 'csv':
         for p in tqdm(target_path):
             station_name = re.findall(r"年(.*)站", p.name)[0] 
@@ -51,9 +82,16 @@ def build_station_data(year:str, ret:Dict) -> Dict[str, Dict[datetime, Dict[str,
             for row in csv.iterrows():
                 date = datetime.strptime(row[1][0], "%Y/%m/%d").date() 
                 key = row[1][2]
-                if(ret.get(station_name).get(date) == None):
-                    ret[station_name][date] = {}
-                ret[station_name][date][key] = list(row[1][3:])
+                if _date != date:
+                    record = {}
+                    for _attr in attr:
+                        record[_attr] = [-1] * 24
+                #  if(ret.get(station_name).get(date) == None):
+                    #  ret[station_name][date] = {}
+                record[key] = list(row[1][3:])
+                ret[station_name][date] = record
+                #  ret[station_name][date][key] = list(row[1][3:])
+                _date = date
     return ret
 
 def save_pickle(path, obj):
@@ -67,7 +105,7 @@ def load_pickle(path):
     return pickle.load(path.open('rb'))
 
 if __name__ == '__main__':
-    start_year = 104 
-    end_year = 104 
+    start_year = 89 
+    end_year = 106 
     d = build_station_data_by_year(start_year, end_year)
-    save_pickle('../pickle_data/epa-104.pickle', d)
+    save_pickle('../pickle_data/epa-89-106.pickle', d)
